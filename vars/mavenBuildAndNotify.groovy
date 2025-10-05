a26e09f27e34c7d05bbc3d26e2afb117f534550c
hardbro786 <126283577+hardbro786@users.noreply.github.com>
2025-10-05 22:33:27 +0530

    Update mavenBuildAndNotify.groovy

--- vars/mavenBuildAndNotify.groovy
+++ vars/mavenBuildAndNotify.groovy
@@ -1,21 +1,11 @@
-def call(Map config = [:]) {
-    script {
-        echo "🔹 Running Maven command: ${config.mavenCommand ?: 'clean package'}"
-        sh "${config.mavenCommand ?: 'mvn clean package'}"
-
-        // Email notifier instance
-        def notifier = new org.opstree.utils.EmailNotifier(this)
-
-        notifier.mail([
-            to: config.to ?: 'modihardik19@gmail.com',
-            subject: "Maven Build - ${currentBuild.currentResult}: ${config.repo ?: env.GIT_URL}",
-            body: """
-                Job: ${env.JOB_NAME}
-                Build Number: ${env.BUILD_NUMBER}
-                Status: ${currentBuild.currentResult}
-                Repo: ${config.repo ?: env.GIT_URL}
-                Console: ${env.BUILD_URL}
-            """
-        ])
+stage('Maven Build & Notify') {
+    steps {
+        dir("${env.POM_DIR}") {
+            mavenBuildAndNotify(
+                to: EMAIL_TO,
+                mavenCommand: MAVEN_CMD,
+                repo: env.GIT_URL
+            )
+        }
     }
 }
