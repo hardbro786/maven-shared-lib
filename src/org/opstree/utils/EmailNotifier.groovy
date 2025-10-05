705edef8546566388f02fce08a4c389b6f9fb32b
hardbro786 <126283577+hardbro786@users.noreply.github.com>
2025-10-05 22:11:08 +0530

    Update EmailNotifier.groovy

--- src/org/opstree/utils/EmailNotifier.groovy
+++ src/org/opstree/utils/EmailNotifier.groovy
@@ -1,39 +1,17 @@
 package org.opstree.utils
 
 class EmailNotifier implements Serializable {
+    def script
 
-    def steps  // Jenkins pipeline context
-
-    EmailNotifier(steps) {
-        this.steps = steps
+    EmailNotifier(script) {
+        this.script = script
     }
 
     void mail(Map params) {
-        if (!params.to || !params.subject || !params.body) {
-            steps.error("Missing required parameters for EmailNotifier.mail()")
-        }
-
-        // Jenkins mail step through pipeline context
-        steps.mail(
+        script.emailext(
             to: params.to,
             subject: params.subject,
             body: params.body
         )
-    }
-
-    void success(Map params) {
-        mail([
-            to: params.to,
-            subject: "SUCCESS: ${params.subject}",
-            body: params.body
-        ])
-    }
-
-    void failure(Map params) {
-        mail([
-            to: params.to,
-            subject: "FAILURE: ${params.subject}",
-            body: params.body
-        ])
     }
 }
